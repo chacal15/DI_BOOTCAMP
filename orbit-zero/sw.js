4fdbb1bf5eb021cacb5e80b1fbc526584f9cf316
chacal15 <chacal15@users.noreply.github.com>
2026-08-10 16:00:52 +0000

    Make Orbit Zero installable as a mobile app

---
+++ orbit-zero/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "orbit-zero-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./manifest.webmanifest",
+  "./orbit-zero-logo.svg",
+  "./js/main.js",
+  "./js/game.js",
+  "./js/particles.js",
+  "./js/audio.js",
+  "./js/storage.js",
+  "./js/utils.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)),
+    )),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request)),
+  );
+});
